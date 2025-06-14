59f8b6e660669e462ca92bdbb61f9850cf1a39c5
Abdulsamet TÜTÜNCÜ <61270806+abdupa61@users.noreply.github.com>
2025-06-14 19:59:34 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,35 @@
 "use client";
-
 import { UploadButton, UploadDropzone } from "@/src/utils/uploadthing";
-
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
+  
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
+      {/* Hoş geldin mesajı */}
+      <div className="mb-12 text-center">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">
+          Abdulsamet ❤️ Zehra'nın Düğününe Hoşgeldiniz
+        </h1>
+        <p className="text-lg text-gray-600">
+          Düğün fotoğraflarınızı yükleyebilirsiniz
+        </p>
+      </div>
+
       {/* <UploadButton
         endpoint="imageUploader"
         onClientUploadComplete={(res) => {
           // Do something with the response
           console.log("Files: ", res);
-          alert("Upload Completed");x
+          alert("Upload Completed");
         }}
         onUploadError={(error: Error) => {
           // Do something with the error.
           alert(`ERROR! ${error.message}`);
         }}
       /> */}
-
+      
       <UploadDropzone
         endpoint="imageUploader"
         onClientUploadComplete={(res) => {
@@ -30,13 +39,8 @@ export default function Home() {
           alert(`ERROR! ${error.message}`);
         }}
       />
+      
 
-      <a
-        href="https://github.com/theskillwithin/upload-a-thing"
-        className="mt-8 hover:text-blue-700 text-blue-500"
-      >
-        https://github.com/theskillwithin/upload-a-thing
-      </a>
     </main>
   );
 }
